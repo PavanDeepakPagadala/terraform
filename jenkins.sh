43698829468446644a667dd237e196c8dd652833
Pavan Deepak <pavandeepak007@outlook.com>
2023-07-04 09:58:21 +0530

    final

--- jenkins.sh
+++ jenkins.sh
@@ -40,6 +40,21 @@ echo 'jenkins ALL=(ALL) NOPASSWD: ALL' | tee /etc/sudoers.d/jenkins
 chmod 440 /etc/sudoers.d/jenkins
 chown root:root /etc/sudoers.d/jenkins
 
+# Specify the desired values for JAVA_HOME, M2, and M2_HOME
+JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.342.b07-1.amzn2.0.1.x86_64"
+M2="/opt/maven"
+M2_HOME="/opt/maven/bin"
+
+# Update the .bash_profile file
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bash_profile
+echo "export M2=$M2" >> ~/.bash_profile
+echo "export M2_HOME=$M2_HOME" >> ~/.bash_profile
+
+# Update the current shell session with the new environment variables
+export JAVA_HOME=$JAVA_HOME
+export M2=$M2
+export M2_HOME=$M2_HOME
+
 # Start Jenkins and Tomcat services
 systemctl start jenkins
 systemctl enable jenkins
